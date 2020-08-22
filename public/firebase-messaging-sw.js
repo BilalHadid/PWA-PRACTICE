3f15f5e83dc06f0f223492159a73589169c8df13
bilalhadid <hadid0648@gmail.com>
2020-08-23 00:57:52 +0500

    Send Message

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts("https://www.gstatic.com/firebasejs/7.19.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/7.19.0/firebase-messaging.js"
+);
+firebase.initializeApp({
+  apiKey: "AIzaSyCR_vyJA8IlA8hm00wKFHNGrTSZRI-LFr4",
+  authDomain: "notification-app-59f73.firebaseapp.com",
+  databaseURL: "https://notification-app-59f73.firebaseio.com",
+  projectId: "notification-app-59f73",
+  storageBucket: "notification-app-59f73.appspot.com",
+  messagingSenderId: "233609071898",
+  appId: "1:233609071898:web:a09b448acb0844670b30a3",
+});
+firebase.messaging();
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyCR_vyJA8IlA8hm00wKFHNGrTSZRI-LFr4",
+  authDomain: "notification-app-59f73.firebaseapp.com",
+  databaseURL: "https://notification-app-59f73.firebaseio.com",
+  projectId: "notification-app-59f73",
+  storageBucket: "notification-app-59f73.appspot.com",
+  messagingSenderId: "233609071898",
+  appId: "1:233609071898:web:a09b448acb0844670b30a3",
+};
+firebase.initializeApp(config);
+
+export default firebase;
